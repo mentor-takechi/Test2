2ffb490a763bb2569d32b5f19a5595c2e545b99f
mentor-takechi <mentor-takechi@takechansoft.com>
2020-05-23 16:50:06 +0900

    Initial Commit

---
+++ Test2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test2
+//
+//  Created by take on 2020/05/23.
+//  Copyright © 2020 taro.kirameki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
